f307a542f4eec97a9743967d3ace4806f3943e5c
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-17 00:03:25 +0200

    users <> creating check_password function

--- Users.php
+++ Users.php
@@ -8,16 +8,29 @@ class User{
         $this->name = $name;
         $this->password = $password;
     }
+    public static function check_password($password){
+        if(
+            strlen($password) >= 12 &&
+            preg_match('/[A-Z]/', $password) &&
+            preg_match('/[a-z]/', $password) &&
+            preg_match('/[\W_]/', $password)
+        )
+        return True;
+        else
+        return False;
+    }
 }
 $name=$_POST["name"] ?? null;
 $password = $_POST["password"] ?? null;
 if(is_null($name) || is_null($password)){
     echo json_encode([
-        "message"=>"missing name or password"
+        "Strong Password: "=>check_password($password)
     ]);
 }
 else{
-
+    echo json_encode([
+        "Strong Password: "=>User::check_password($password)
+    ]);
     $user1 = new User($name, $password);
     
     echo json_encode([
