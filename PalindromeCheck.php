f8680fa8d8e64a5a5304bdb11f15ba1248b0f44c
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-16 16:27:18 +0200

    palindrome <> returning result of palindrome

--- PalindromeCheck.php
+++ PalindromeCheck.php
@@ -1,10 +1,16 @@
 <?php
 
 function isPalindrome($word){
-
-    return False;
+    $wordArr = str_split($word);
+    $reversedWord = "";
+    for($i = strlen($word)-1 ; $i>=0;$i--)
+        $reversedWord .= $wordArr[$i];
+    
+    
+    return $reversedWord == $word;
 }
+$word="madam";
 
-$result = isPalindrome("hello");
+$result = isPalindrome($word);
 
-echo $result ? json_encode(["Result: "=>"Palindrome"]) : json_encode(["Result: "=>"Not Palindrome"]) ;
+echo $result ? json_encode(["Result: "=>"$word is Palindrome"]) : json_encode(["Result: "=>"$word is not Palindrome"]) ;
