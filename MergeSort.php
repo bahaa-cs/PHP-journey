1d4b5129a987f0453160b0850c4f347c47c601e9
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-16 17:46:38 +0200

    mergeSort <> creating merge function

--- MergeSort.php
+++ MergeSort.php
@@ -1,29 +1,49 @@
 <?php
 
-function generateArray($length){
-    for($i = 0; $i<$length;$i++){
-        $unsortedArray[] = rand(1,$length);
+function generateArray($length) {
+    $unsortedArray = [];
+    for ($i = 0; $i < $length; $i++) {
+        $unsortedArray[] = rand(1, $length);
     }
     return $unsortedArray;
 }
 
-$unsortedArray = generateArray(15);
+function split(&$arr, $startIndex, $endIndex, &$result) {
+    if ($endIndex - $startIndex <= 1) {
+        return;
+    }
 
+    $middleIndex = intval(($startIndex + $endIndex) / 2);
 
-function split($arr,$startIndex,$endIndex,$result){
-    if ($indexEnd - $indexStart <=1)
-    return
+    split($result, $startIndex, $middleIndex, $arr);
+    split($result, $middleIndex, $endIndex, $arr);
 
-    $middleIndex = intval(($startIndex - $endIndex)/2)
-
-    split($result,$startIndex,$middleIndex,$arr)
-    split($result,$middleIndex,$endIndex,$arr)
-
-    merge($arr, $startIndex, $middleIndex, $endIndex,$result)
+    merge($arr, $startIndex, $middleIndex, $endIndex, $result);
 }
 
+function merge(&$arr, $startIndex, $middleIndex, $endIndex, &$result) {
+    $start = $startIndex;
+    $middle = $middleIndex;
+    for ($i = $startIndex; $i < $endIndex; $i++) {
+        if ($start < $middleIndex && ($middle >= $endIndex || $arr[$start] <= $arr[$middle])) {
+            $result[$i] = $arr[$start];
+            $start++;
+        } else {
+            $result[$i] = $arr[$middle];
+            $middle++;
+        }
+    }
+}
 
+$unsortedArray = generateArray(15);
 
+$result = $unsortedArray;
 
-echo json_encode(["unsorted array: "=>$unsortedArray]);
+split($unsortedArray, 0, count($unsortedArray), $result);
 
+echo json_encode([
+    "unsorted array" => $unsortedArray,
+    "sorted array" => $result
+]);
+
+?>
